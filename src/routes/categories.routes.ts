c4b9f48c0c4e315902553d9b6c579fd16bd268c2
Kelvin Henrique <keelviinn@gmail.com>
2021-10-24 11:58:40 -0300

    rotas de criaçao e listagem de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,21 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  });
+  return response.status(201).send();
+})
 
-  categories.push(category);
+categoriesRoutes.get("/", (request, response) => {
+  const categories = categoriesRepository.list();
 
-  return response.status(201).json({ category });
+  return response.json(categories);
 })
 
 export { categoriesRoutes };
