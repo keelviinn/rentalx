1725bd23b07466d8b55e763900344984e1c8dad1
Kelvin Henrique <keelviinn@gmail.com>
2021-10-24 12:06:57 -0300

    findByName

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,15 +7,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
-})
+});
 
 categoriesRoutes.get("/", (request, response) => {
   const categories = categoriesRepository.list();
 
   return response.json(categories);
-})
+});
 
 export { categoriesRoutes };
